3e5b0ce4ce6d759751fe54d601fff826a63627a4
AshutoshG1 <ashutoshgautamspn@gmail.com>
2024-12-06 18:29:10 +0530

    committed

---
+++ src/main/java/com/codewithashutosh/controller/ProductCheckoutController.java
@@ -0,0 +1,32 @@
+package com.codewithashutosh.controller;
+
+
+import com.codewithashutosh.dto.ProductRequest;
+import com.codewithashutosh.dto.StripeResponse;
+import com.codewithashutosh.service.StripeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product/v1")
+public class ProductCheckoutController {
+
+
+    private StripeService stripeService;
+
+    public ProductCheckoutController(StripeService stripeService) {
+        this.stripeService = stripeService;
+    }
+
+    @PostMapping("/checkout")
+    public ResponseEntity<StripeResponse> checkoutProducts(@RequestBody ProductRequest productRequest) {
+        StripeResponse stripeResponse = stripeService.checkoutProducts(productRequest);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(stripeResponse);
+    }
+}
